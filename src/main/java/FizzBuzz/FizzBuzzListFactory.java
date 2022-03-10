6cb2dd594e418ada03b2b1db1ae8b96bcf068733
salimaYagoub <s.yagoubi@slokware.com>
2022-03-10 18:01:08 +0100

    Given list with element from 1 to N when given number N

--- src/main/java/FizzBuzz/FizzBuzzListFactory.java
+++ src/main/java/FizzBuzz/FizzBuzzListFactory.java
@@ -2,10 +2,12 @@ package FizzBuzz;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBuzzListFactory {
     public static List listFactory(int i) {
-        if (i == 1) return Arrays.asList(1);
-        else return Arrays.asList(1,2);
+        return IntStream.range(1, i+1).boxed().collect(Collectors.toList());
+
     }
 }
--- src/test/java/FizzBuzzListFactoryTest.java
+++ src/test/java/FizzBuzzListFactoryTest.java
@@ -20,5 +20,19 @@ public class FizzBuzzListFactoryTest {
         List actualList = FizzBuzzListFactory.listFactory(2);
         Assert.assertEquals(expectedList,actualList);
     }
+
+    @Test
+    public void ShouldReturnListWithElementFrom1To6WhenGiven6(){
+        List expectedList = Arrays.asList(1,2,3,4,5,6);
+        List actualList = FizzBuzzListFactory.listFactory(6);
+        Assert.assertEquals(expectedList,actualList);
+    }
+
+    @Test
+    public void ShouldReturnListWithElementFrom1To15WhenGiven15(){
+        List expectedList = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+        List actualList = FizzBuzzListFactory.listFactory(15);
+        Assert.assertEquals(expectedList,actualList);
+    }
 }
 
